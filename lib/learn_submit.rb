09fe4c3be297fe1487aa6eb69d7c9f83987d6bd5
Logan Hasson <logan.hasson@gmail.com>
2015-05-27 17:55:10 -0400

    Change require order

--- lib/learn_submit.rb
+++ lib/learn_submit.rb
@@ -1,11 +1,12 @@
+require 'timeout'
+require 'netrc'
+require 'git'
+require 'learn_web'
+
 require 'learn_submit/version'
 require 'learn_submit/submission'
 require 'learn_submit/submission/git_interactor'
 
-require 'netrc'
-require 'git'
-require 'learn_web'
-require 'timeout'
 
 module LearnSubmit
 end
